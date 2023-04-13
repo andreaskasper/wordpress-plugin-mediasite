4a37e6af83699737cc5b18e334dac31ecb667aee
Andreas Kasper <andreas.kasper@goo1.de>
2023-04-13 14:51:33 +0200

    Forgotten Asset

--- src/goo1-elementor-mediasite/goo1-elementor-mediasite.php
+++ src/goo1-elementor-mediasite/goo1-elementor-mediasite.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://github.com/andreaskasper/
  * Description: Create a mediasite with Elementor for dancers and others to sell videos and media.
  * Author: Andreas Kasper
- * Version: 0.1.106
+ * Version: 0.1.107
  * Author URI: https://github.com/andreaskasper/
  * Network: True
  * Text Domain: goo1-elementor-mediasite
