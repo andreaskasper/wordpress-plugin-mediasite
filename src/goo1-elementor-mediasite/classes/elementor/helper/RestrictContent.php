575c3af2b213da78be47c96db94598cdaedee68c
Andreas Kasper <Andreas.Kasper@goo1.de>
2023-07-11 12:27:33 +0000

    REstrict Content without Warning

--- src/goo1-elementor-mediasite/classes/elementor/helper/RestrictContent.php
+++ src/goo1-elementor-mediasite/classes/elementor/helper/RestrictContent.php
@@ -103,7 +103,8 @@ class RestrictContent {
 
         $repeater = new \Elementor\Repeater();
 
-		$rows = $wpdb->get_results( $wpdb->prepare('SELECT ID, post_title FROM '.$wpdb->posts.' WHERE post_type IN ("product","product_variation")  ORDER BY post_title ASC',array()), ARRAY_A );
+		//$rows = $wpdb->get_results( $wpdb->prepare('SELECT ID, post_title FROM '.$wpdb->posts.' WHERE post_type IN ("product","product_variation")  ORDER BY post_title ASC'), ARRAY_A );
+        $rows = $wpdb->get_results( 'SELECT ID, post_title FROM '.$wpdb->posts.' WHERE post_type IN ("product","product_variation")  ORDER BY post_title ASC', ARRAY_A );
 
 		$w = array();
 		foreach($rows as $row) {
@@ -157,6 +158,7 @@ class RestrictContent {
 			]
 		);
 
+        $repeater = new \Elementor\Repeater();
         $w = self::get_subscription_products_array();
 
         $repeater->add_control(
@@ -254,7 +256,8 @@ class RestrictContent {
 		);
 
         
-        $rows = $wpdb->get_results( $wpdb->prepare('SELECT ID, post_title FROM '.$wpdb->posts.' WHERE post_type IN ("elementor_library")  ORDER BY post_title ASC',array()), ARRAY_A );
+        //$rows = $wpdb->get_results( $wpdb->prepare('SELECT ID, post_title FROM '.$wpdb->posts.' WHERE post_type IN ("elementor_library")  ORDER BY post_title ASC'), ARRAY_A );
+        $rows = $wpdb->get_results( 'SELECT ID, post_title FROM '.$wpdb->posts.' WHERE post_type IN ("elementor_library")  ORDER BY post_title ASC', ARRAY_A );
 
 		$rows_templates = array();
 		foreach($rows as $row) {
@@ -552,7 +555,6 @@ class RestrictContent {
         foreach ( $products as $product ) {
             // Check if the product is a subscription product
             if ( class_exists( 'WC_Subscriptions_Product' ) && \WC_Subscriptions_Product::is_subscription( $product ) ) {
-            	//if ($product->is_type('variation')) $subscription_products[$product->ID] = "___".$product->post_title;
                 $subscription_products[$product->ID] = $product->post_title;
             }
         }
@@ -581,16 +583,11 @@ class RestrictContent {
                 $subscription_product_id = $item->get_product_id();
 
                 // To get the variation subscription product ID
-                $variation_id = $item->get_variation_id();
-                
-                //echo("PID: ".$subscription_product_id." ".$variation_id);
+                //$variation_id = $item->get_variation_id();
 
                 // Or to get the simple subscription or the variation subscription product ID
                 //$_product_id = $product->get_id();
                 if ( $subscription_product_id == $product_id ) {
-                    return true; // Subscription is active for the current user and product
-                }
-                if ( $variation_id == $product_id ) {
                     return true; // Subscription is active for the current user and product
                 }
             }
